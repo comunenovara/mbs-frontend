adc7f0df42df3ba4d2e2e9fb081da4447e46019f
Cavallaro Catello Stefano <stefano@stepo.biz>
2022-12-19 18:15:58 +0100

    Cleaned project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,21 @@ import { ExtraOptions, Route, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppLayoutComponent } from "./layout/prime-sakai/components/main/app.layout.component";
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 
 const routes: Route[] = [
     {
         path: '',
         component: AppLayoutComponent,
         children: [
-            { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-            { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
-            { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
-            { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
-            { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-            { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) },
-            { path: 'mbs', loadChildren: () => import('./mbs/mbs.module').then(m => m.MbsModule) },
+            {
+                path: '',
+                pathMatch : 'full',
+                redirectTo: 'enzo/asset/list',
+            },
             { path: 'enzo', loadChildren: () => import('./modules/enzo-mbs-main/mbs-main.module').then(m => m.EnzoMbsMainModule) }
         ]
     },
-    { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-    { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-    { path: 'notfound', component: NotfoundComponent },
+//    { path: 'notfound', component: NotfoundComponent },
     { path: '**', redirectTo: '/notfound' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { PrimeSakaiLayoutModule } from './layout/prime-sakai/app.layout.module';
 
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -31,11 +23,9 @@ import { HttpClientModule } from '@angular/common/http';
 
     ],
     providers: [
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
+
     ],
     declarations: [
-        NotfoundComponent,
         AppComponent,
     ],
     bootstrap: [AppComponent]
